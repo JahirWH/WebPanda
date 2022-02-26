87fcb474c2211b039b7cdbc9dc2be530c2a797e5
Jahir <93404854+JahirWH@users.noreply.github.com>
2022-02-26 09:05:40 -0600

    update index3
    abrir y cerrar ventana en imagenes con informacion acerca de la historia de las imagenes(optimizacion)

--- assets/js/main3.js
+++ assets/js/main3.js
@@ -1,3 +1,10 @@
     window.addEventListener("load",function(){
 	document.getElementById("loader").classList.toggle("loader2")
 });
+
+  let d1 = document.getElementById("div1");
+  d1.onclick= alert1;
+
+  function alert1(){
+  	
+  }
