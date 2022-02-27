812c4d9d3e586d16477ee8be68f81d0eeb48dbe8
Jahir <93404854+JahirWH@users.noreply.github.com>
2022-02-27 13:36:51 -0600

    big update
    ctualizacion de a a div con button y modal falta correccion de tamanos de marcos de los divs

--- assets/js/main3.js
+++ assets/js/main3.js
@@ -2,9 +2,20 @@
 	document.getElementById("loader").classList.toggle("loader2")
 });
 
-  let d1 = document.getElementById("div1");
-  d1.onclick= alert1;
 
-  function alert1(){
-  	
-  }
+function modal1(){
+ 	var d1 = document.getElementById("modal_1");
+
+ 	if (d1.style.display == "none" ) {
+ 		d1.style.display = "block";
+ 	}else{
+ 		d1.style.display = "none";
+ 	}
+ }
+
+/*function close(){
+ var D1	= document.getElementById("modal_1");
+	if (D1.style.display == "block") {
+		D1.style.display = "none";
+	}
+}*/
