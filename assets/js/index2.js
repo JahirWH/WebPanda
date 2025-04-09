717f648b79187703d9bb76abe4012fc06410e5e6
Jahir <93404854+JahirWH@users.noreply.github.com>
2025-04-09 17:21:46 -0600

    Restructurar

--- assets/js/index2.js
+++ assets/js/index2.js
@@ -2,8 +2,3 @@
  	document.getElementById("loader").classList.toggle("loader2")
  });
  
- let buscador = document.getElementById("buscar");
-
-  function boton(){
- 	document.write(buscador ="")
- };
